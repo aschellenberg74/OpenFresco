f16218d275e273e64bcdc975753d7ba154d715c9
aschellenberg <aschellenberg@187af744-7923-0410-8f01-f88a99ace7d3>
2007-10-07 00:30:38 +0000

    initial import into repository

---
+++ trunk/EXAMPLES/Cantilever/Cantilever_Adapter.tcl
@@ -0,0 +1,124 @@
+# File: Cantilever_Adapter.tcl (use with Cantilever_Main.tcl)
+#
+# $Revision: $
+# $Date: $
+# $URL: $
+#
+# Written: Andreas Schellenberg (andreas.schellenberg@gmx.net)
+# Created: 09/07
+# Revision: A
+#
+# Purpose: this file contains the tcl input to perform
+# a hybrid simulation of a simple cantilever column
+# with one experimental beamColumn element.
+# Either the actuator or adpater element is used to
+# communicate with the main FE-software which is
+# coordinating and executing the direct integration
+# analysis.
+
+
+# ------------------------------
+# Start of model generation
+# ------------------------------
+# create ModelBuilder (with two-dimensions and 2 DOF/node)
+model BasicBuilder -ndm 2 -ndf 3
+
+# Define geometry for model
+# -------------------------
+# node $tag $xCrd $yCrd
+node  1     0.0    0.0
+node  2     0.0   54.0
+node  3   -10.0   54.0;  # for use with actuator element
+
+# set the boundary conditions
+# fix $tag $DX $DY $RZ
+fix 1   1  1  1
+fix 3   1  1  1;  # for use with actuator element
+
+# Define materials
+# ----------------
+# uniaxialMaterial Steel02 $matTag $Fy $E $b $R0 $cR1 $cR2 $a1 $a2 $a3 $a4 
+uniaxialMaterial Elastic 1 [expr 2.26*29000]
+uniaxialMaterial Steel02 2 [expr 1.5*54] 146966.4 0.01 18.5 0.925 0.15 0.0 1.0 0.0 1.0
+
+# Define sections
+# ---------------
+# section Elastic $secTag $E $A $Iz
+#section Elastic 1 29000 2.26 5.067806896551724
+# section Aggregator $secTag $matTag1 $string1 $matTag2 $string2
+section Aggregator 1 1 P 2 Mz
+
+# Define coordinate transformation
+# --------------------------------
+# geomTransf Linear $transfTag
+geomTransf Linear 1
+
+# Define elements
+# ---------------
+# element nonlinearBeamColumn $eleTag $iNode $jNode $numIntgrPts $secTag $transfTag
+element nonlinearBeamColumn 1 1 2 5 1 1
+
+# element actuator $eleTag $iNode $jNode EA ipPort <-rho rho>
+element actuator 2 3 2 1000000 44000
+
+# element adapter eleTag -node Ndi Ndj ... -dof dofNdi -dof dofNdj ... -stif Kij ipPort <-mass Mij>
+#element adapter 2 -node 2 -dof 1 -stif 100000 44000
+# ------------------------------
+# End of model generation
+# ------------------------------
+
+
+# ------------------------------
+# Start of analysis generation
+# ------------------------------
+# create the system of equations
+system BandGeneral
+
+# create the DOF numberer
+numberer Plain
+
+# create the constraint handler
+constraints Plain
+
+# create the convergence test
+test NormDispIncr 1.0e-8 25
+#test NormUnbalance 1.0e-8 25
+#test EnergyIncr 1.0e-8 25
+
+
+# create the integration scheme
+integrator LoadControl 1.0
+
+# create the solution algorithm
+algorithm Newton
+
+# create the analysis object 
+analysis Static
+# ------------------------------
+# End of analysis generation
+# ------------------------------
+
+
+# ------------------------------
+# Start of recorder generation
+# ------------------------------
+# create the recorder objects
+recorder Node -file Adap_Node_Dsp.out -time -node 2 -dof 1 2 3 disp
+recorder Node -file Adap_Node_Vel.out -time -node 2 -dof 1 2 3 vel
+recorder Node -file Adap_Node_Acc.out -time -node 2 -dof 1 2 3 accel
+
+recorder Element -file Adap_Elmt_Frc.out  -time -ele 1 2 forces
+recorder Element -file Adap_Elmt_tDef.out -time -ele   2 targetDisplacements
+recorder Element -file Adap_Elmt_mDef.out -time -ele   2 measuredDisplacements
+# --------------------------------
+# End of recorder generation
+# --------------------------------
+
+
+# ------------------------------
+# Finally perform the analysis
+# ------------------------------
+analyze 1600
+# --------------------------------
+# End of analysis
+# --------------------------------
---
+++ trunk/EXAMPLES/OneBayFrame/TestLabVIEW/OneBayFrame_Server.tcl
@@ -0,0 +1,65 @@
+# File: OneBayFrame_Server.tcl (use with OneBayFrame_Client.tcl)
+#
+# $Revision: $
+# $Date: $
+# $URL: $
+#
+# Written: Andreas Schellenberg (andreas.schellenberg@gmx.net)
+# Created: 03/07
+# Revision: A
+#
+# Purpose: this file contains the tcl input to perform
+# a distributed hybrid simulation of a one bay frame
+# with two experimental twoNodeLink elements.
+# The specimen is simulated using the LabVIEW
+# controller.
+# The experimental setup is on the server side.
+
+
+# ------------------------------
+# Start of model generation
+# ------------------------------
+# create ModelBuilder (with two-dimensions and 2 DOF/node)
+model BasicBuilder -ndm 2 -ndf 2
+
+# Define materials
+# ----------------
+# uniaxialMaterial Steel02 $matTag $Fy $E $b $R0 $cR1 $cR2 $a1 $a2 $a3 $a4 
+uniaxialMaterial Elastic 1 2.8655
+#uniaxialMaterial Steel02 1 1.5 2.8 0.01 18.5 0.925 0.15 0.0 1.0 0.0 1.0
+
+# Define control points
+# ---------------------
+# expControlPoint tag nodeTag dir resp <-fact f> <-lim l u> ...
+expControlPoint 1 1  ux disp -fact 0.003 -lim -0.01 0.01
+expControlPoint 2 1  ux disp -fact 0.003 ux force -fact [expr 18.0/7.0]
+
+# Define experimental control
+# ---------------------------
+# expControl LabVIEW tag ipAddr <ipPort> -trialCP cpTags -outCP cpTags
+expControl LabVIEW 1 "127.0.0.1" 11997  -trialCP 1  -outCP 2;  # use with NEES-SAM
+#expControl LabVIEW 1 "130.126.242.175" 44000  -trialCP 1  -outCP 2;  # use with Mini-Most at UIUC
+#expControl SimUniaxialMaterials 1 1;  # use for simulation
+
+# Define experimental setup
+# -------------------------
+# expSetup OneActuator $tag <-control $ctrlTag> $dir <-ctrlDispFact $f> ...
+expSetup OneActuator 1 -control 1 1
+
+# Define experimental site
+# ------------------------
+# expSite ActorSite $tag -setup $setupTag $ipPort <-ssl>
+expSite ActorSite 1 -setup 1 8090
+# ------------------------------
+# End of model generation
+# ------------------------------
+
+
+# ------------------------------
+# Start the server process
+# ------------------------------
+# startLabServer $siteTag
+startLabServer  1
+# --------------------------------
+# End of analysis
+# --------------------------------
