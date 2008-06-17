e9bb7e0e164ccbd9b4daca412e52a107a783937c
hongkim <hongkim423@gmail.com>
2008-06-17 04:20:17 +0000

    Adding SimCor example

---
+++ EXAMPLES/OneBayFrame/SimCor/02_Beam_OpenSees/Beam.tcl
@@ -0,0 +1,58 @@
+# File: Beam.tcl
+#
+# $Revision: $
+# $Date: $
+# $URL: $
+#
+# Written: Hong Kim (hongkim@berkeley.edu)
+# Created: 1/08
+# Revision: A
+#
+# Purpose: this file contains the tcl input to perform
+# a distributed hybrid simulation with SimCor of a one bay frame
+# example with two experimental two-node link elements.
+# The beam is modeled in OpenSees
+
+# _____________________________________________________________________________
+#
+# Start of model generation
+# _____________________________________________________________________________
+
+# Create ModelBuilder (with three-dimensions and 6 DOF/node)
+model BasicBuilder -ndm 2
+
+# --------------------------------------------------------------------
+#    Create nodes
+# --------------------------------------------------------------------
+# Node 3 and 4 are connected with spring with large stiffness.
+
+#    tag        X         Y      
+node  1       0.0       0.0      
+node  2     100.0       0.0      
+
+# Controlling node
+
+# --------------------------------------------------------------------
+#     Boundary condition
+# --------------------------------------------------------------------
+# Fix supports at ends of truss
+#    tag   DX   DY   RZ 
+fix   1    0    1    0 
+fix   2    0    1    0 
+
+# --------------------------------------------------------------------
+#     Material property
+# --------------------------------------------------------------------
+# Define materials
+# ----------------
+uniaxialMaterial Elastic 1 [expr 2.0*100.0/1.0]
+
+# Define truss elements
+# -------------------------
+# spring
+# element truss $eleTag $iNode $jNode $A $matTag
+element truss 1 1 2 1.0 1
+
+# ------------------------------
+# End of model generation
+# ------------------------------
---
+++ EXAMPLES/OneBayFrame/SimCor/02_Beam_OpenSees/StaticAnalysisEnv.tcl
@@ -0,0 +1,31 @@
+# File: StaticAnalysisEnv.tcl
+#
+# $Revision: $
+# $Date: $
+# $URL: $
+#
+# Written: Hong Kim (hongkim@ce.berkeley.edu)
+# Created: 1/08
+# Revision: A
+#
+# Purpose: this file is require to run OpenSees using SimCore
+# Refer to the SimcCor manual for more information.
+
+system BandGeneral
+# system SparseGeneral
+# system BandSPD                
+ 
+# constraints Transformation    
+constraints Penalty 1E20 1E20
+numberer Plain                
+
+#test EnergyIncr 1.0e-12  20 0 
+test NormDispIncr 1.0e-5 10 0
+
+#algorithm Newton              
+algorithm ModifiedNewton
+
+integrator LoadControl 1      
+analysis Static               
+		
+
