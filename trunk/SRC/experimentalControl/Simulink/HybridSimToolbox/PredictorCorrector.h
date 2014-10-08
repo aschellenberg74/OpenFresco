90aacd0db85bc327545a008da80e01efbf3e691b
aschell <aschell@187af744-7923-0410-8f01-f88a99ace7d3>
2014-10-08 15:18:54 +0000

    renamed variables to be more generic

--- trunk/SRC/experimentalControl/Simulink/HybridSimToolbox/PredictorCorrector.h
+++ trunk/SRC/experimentalControl/Simulink/HybridSimToolbox/PredictorCorrector.h
@@ -15,78 +15,78 @@
 // methods to initialize and zero data arrays
 int initData(int nDOF, double dtCon, double dtSim, double dtInt);
 int zeroData();
-int zeroDsp(double *dsp);
+int zeroSig(double *sig);
 
 // methods to update the data arrays
-int setCurDsp(double *dsp, double x);
-int setNewDsp(double *dsp);
-int setNewVel(double *vel);
-int setNewAcc(double *acc);
+int setCurSig(double *sig, double x);
+int setNewSig(double *sig);
+int setNewSigDot(double *sigDot);
+int setNewSigDotDot(double *sigDotDot);
 
 // methods to get data and parameters
-double getNumSubSteps(double vDOF, int dofID, double vDOFmax, int Nmin);
+double getNumSubSteps(double sigDotDOF, int dofID, double sigDotDOFmax, int Nmin);
 
 // method for hold in ramp-and-hold procedure
-int predictP0(double *dsp, double x);
+int predictP0(double *sig, double x);
 
 // methods using linear Lagrange polynomials
-int predictP1(double *dsp, double x);
-int correctP1(double *dsp, double x);
+int predictP1(double *sig, double x);
+int correctP1(double *sig, double x);
 
 // methods using quadratic Lagrange polynomials
-int predictP2(double *dsp, double x);
-int correctP2(double *dsp, double x);
+int predictP2(double *sig, double x);
+int correctP2(double *sig, double x);
 
 // methods using cubic Lagrange polynomials
-int predictP3(double *dsp, double x);
-int correctP3(double *dsp, double x);
+int predictP3(double *sig, double x);
+int correctP3(double *sig, double x);
 
 // method for hold in ramp-and-hold procedure (same as predictP0)
-int predictD0(double *dsp, double x);
+int predictD0(double *sig, double x);
 
-// methods using linear Lagrange polynomials and current disp
+// methods using linear Lagrange polynomials and current signal
 // when switching between prediction and correction
-int predictD1(double *dsp, double x);
-int predictV1(double *vel, double x);
-int predictA1(double *acc, double x);
-int correctD1(double *dsp, double x);
-int correctV1(double *vel, double x);
-int correctA1(double *acc, double x);
+int predictD1(double *sig, double x);
+int predictV1(double *sigDot, double x);
+int predictA1(double *sigDotDot, double x);
+int correctD1(double *sig, double x);
+int correctV1(double *sigDot, double x);
+int correctA1(double *sigDotDot, double x);
 
-// methods using quadratic Lagrange polynomials and current disp
+// methods using quadratic Lagrange polynomials and current signal
 // when switching between prediction and correction
-int predictD2(double *dsp, double x);
-int predictV2(double *vel, double x);
-int predictA2(double *acc, double x);
-int correctD2(double *dsp, double x);
-int correctV2(double *vel, double x);
-int correctA2(double *acc, double x);
+int predictD2(double *sig, double x);
+int predictV2(double *sigDot, double x);
+int predictA2(double *sigDotDot, double x);
+int correctD2(double *sig, double x);
+int correctV2(double *sigDot, double x);
+int correctA2(double *sigDotDot, double x);
 
-// methods using cubic Lagrange polynomials and current disp
+// methods using cubic Lagrange polynomials and current signal
 // when switching between prediction and correction
-int predictD3(double *dsp, double x);
-int predictV3(double *vel, double x);
-int predictA3(double *acc, double x);
-int correctD3(double *dsp, double x);
-int correctV3(double *vel, double x);
-int correctA3(double *acc, double x);
+int predictD3(double *sig, double x);
+int predictV3(double *sigDot, double x);
+int predictA3(double *sigDotDot, double x);
+int correctD3(double *sig, double x);
+int correctV3(double *sigDot, double x);
+int correctA3(double *sigDotDot, double x);
 
-// methods using cubic Hermite polynomials and current disp
+// methods using cubic Hermite polynomials and current signal
 // when switching between prediction and correction
-int predictDV(double *dsp, double x);
-int correctDV(double *dsp, double x);
+int predictDV(double *sig, double x);
+int correctDV(double *sig, double x);
 
-// methods using disp, vel and accel and current disp
+// methods using sig, sigDot, sigDotDot and current signal
 // when switching between prediction and correction
-int predictDVA(double *dsp, double x);
-int correctDVA(double *dsp, double x);
+int predictDVA(double *sig, double x);
+int correctDVA(double *sig, double x);
 
 // global variables
 extern int i, nDOF;
 extern double dtCon, dtSim, dtInt, xi;
 
-extern double *dsp1, *dsp2, *dsp3, *dsp4, *dsp5, *dsp6, *dspXi;
-extern double *vel1, *vel2;
-extern double *acc1;
+extern double *sig1, *sig2, *sig3, *sig4, *sig5, *sig6, *sigXi;
+extern double *sigDot1, *sigDot2;
+extern double *sigDotDot1;
 
 #endif
