80c23d36f58fbabbace897a35be2df4587332290
aschell <aschell@187af744-7923-0410-8f01-f88a99ace7d3>
2014-10-08 16:28:45 +0000

    bugfix

--- trunk/SRC/experimentalControl/Simulink/GeneralUtilities/plotOutputDSpace.m
+++ trunk/SRC/experimentalControl/Simulink/GeneralUtilities/plotOutputDSpace.m
@@ -1,22 +1,32 @@
-function data = plotOutputDSpace(fileName,actID,tStart)
+function data = plotOutputDSpace(fileName,dofID,tStart,iDelay)
 %PLOTOUTPUTDSPACE to plot the dSpace output from a hybrid simulation
-% data = plotOutputDSpace(fileName)
+% data = plotOutputDSpace(fileName,dofID,tStart,iDelay)
 %
 % data     : data structure
 % fileName : .mat file to be loaded
-% actID    : id of actuator to plot (optional)
+% dofID    : id of degree-of-freedom to plot (optional)
 % tStart   : time to start plotting (optional)
+% iDelay   : delay compensation in number of samples (optional)
 %
 % Written: Andreas Schellenberg (andreas.schellenberg@gmail.com)
 % Created: 04/05
+%
+% $Revision$
+% $Date$
+% $URL$
 
 %#ok<*TRYNC>
 
 if (nargin<2)
-    actID = 1;
+    dofID = 1;
     id = 1000;
+    iDelay(dofID) = 0;
 elseif (nargin<3)
     id = 1000;
+    iDelay(dofID) = 0;
+elseif (nargin<4)
+    id = max(tStart*1000,1);
+    iDelay(dofID) = 0;
 else
     id = max(tStart*1000,1);
 end
@@ -27,12 +37,16 @@ data = eval(fileName);
 
 time = data.X.Data';
 npts = length(time);
-dt = data.Capture.SamplingPeriod;
+dt = data.Capture.SamplingPeriod*double(data.Capture.Downsampling);
 
 i = ~cellfun(@isempty,strfind({data.Y.Name},'targDsp'));
 targDsp = reshape([data.Y(i).Data],npts,[]);
 i = ~cellfun(@isempty,strfind({data.Y.Name},'commDsp'));
 commDsp = reshape([data.Y(i).Data],npts,[]);
+i = ~cellfun(@isempty,strfind({data.Y.Name},'commVel'));
+commVel = reshape([data.Y(i).Data],npts,[]);
+i = ~cellfun(@isempty,strfind({data.Y.Name},'commAcc'));
+commAcc = reshape([data.Y(i).Data],npts,[]);
 i = ~cellfun(@isempty,strfind({data.Y.Name},'measDsp'));
 measDsp = reshape([data.Y(i).Data],npts,[]);
 i = ~cellfun(@isempty,strfind({data.Y.Name},'measFrc'));
@@ -55,130 +69,168 @@ SS = get(0,'screensize');
 %==========================================================================
 % command displacements
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
+subplot(3,1,1);
 try
-    plot(time(id:end),commDsp(id:end,actID),'-');
+    plot(time(id:end),commDsp(id:end,dofID),'-');
 end
 grid('on');
 xlabel('Time [sec]');
 ylabel('Command Displacement [in.]');
-title(sprintf('Command Displacement from dSpace: Actuator %02d',actID));
+title(sprintf('Command Displacement from dSpace: DOF %02d',dofID));
+
+subplot(3,1,2);
+try
+    plot(time(id:end),commVel(id:end,dofID),'b-');
+    hold on;
+    %plot(time(id:end),commVelF(id:end,dofID),'r--');
+    %commVel2 = [zeros(1,size(commDsp,2)); 1/dt*diff(commDsp,[],1)];
+    %plot(time(id:end),commVel2(id:end,dofID),'r--');
+end
+grid('on');
+xlabel('Time [sec]');
+ylabel('Command Velocity [in./sec]');
+
+subplot(3,1,3);
+try
+    plot(time(id:end),commAcc(id:end,dofID),'b-');
+    hold on;
+    %commAcc2 = [zeros(1,size(commVel2,2)); 1/dt*diff(commVel2,[],1)];
+    %plot(time(id:end),commAcc2(id:end,dofID),'r--');
+end
+grid('on');
+xlabel('Time [sec]');
+ylabel('Command Acceleration [in./sec^2]');
 %==========================================================================
 % target, command and measured displacements
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    plot(time(id:end),targDsp(id:end,actID),'-b');
+   plot(time(id:end),max(commDsp(id:end,dofID))*(counter(id:end)==max(iDelay(dofID),1)),':y');
+   hold('on');
+   plot(time(id:end),min(commDsp(id:end,dofID))*(counter(id:end)==max(iDelay(dofID),1)),':y');
 end
 hold('on');
-try
-    plot(time(id:end),commDsp(id:end,actID),'-r');
-end
-try
-    plot(time(id:end),measDsp(id:end,actID),'-g');
-end
 % try
-%    plot(time(id:end),state(id:end),'-k');
+%    plot(time(id:end),state(id:end),'-y');
 % end
+try
+    ph(1) = plot(time(id:end),targDsp(id:end,dofID),'-b','LineWidth',1.0);
+end
+try
+    ph(2) = plot(time(id:end),commDsp(id:end,dofID),'-r','LineWidth',1.0);
+end
+try
+    ph(3) = plot(time(id:end),measDsp(id:end,dofID),'-g','LineWidth',1.0);
+end
 grid('on');
 xlabel('Time [sec]');
 ylabel('Displacement [in.]');
-title(sprintf('Displacements from dSpace: Actuator %02d',actID));
-legend('target','command','measured');
+title(sprintf('Displacements from dSpace: DOF %02d',dofID));
+legend(ph,'target','command','measured');
 %==========================================================================
 % error between measured and target displacements
-CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
+figure;
+%CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    targID = find(diff(atTarget) > 0) + 1;
+    targID = find(counter == max(iDelay(dofID),1));
     tID = (targID >= id);
     targID = targID(tID);
-    error = measDsp(targID,actID) - targDsp(targID,actID);
-    plot(time(targID),targDsp(targID,actID),'-b');
+    timeInterp = (0:0.001:time(end))';
+    targDspAT = interp1(time(targID),targDsp(targID,:),timeInterp);
+    measDspAT = interp1(time(targID),measDsp(targID,:),timeInterp); 
+    error = measDspAT(:,dofID) - targDspAT(:,dofID);
+    timeShift = getTimeShift(timeInterp,targDspAT(:,dofID),timeInterp,measDspAT(:,dofID));
+    plot(timeInterp(id:end),targDspAT(id:end,dofID),'-b','LineWidth',1.5);
     hold('on');
-    plot(time(targID),measDsp(targID,actID),'-r');
-    plot(time(targID),error,'-g');
+    plot(timeInterp(id:end),measDspAT(id:end,dofID),'-r','LineWidth',1.5);
+    plot(timeInterp(id:end)+timeShift,measDspAT(id:end,dofID),'--m','LineWidth',1.5);
+    plot(timeInterp(id:end),error(id:end),'-g','LineWidth',1.5);
 end
 grid('on');
-xlabel('Time [sec]');
-ylabel('Displacement [in.]');
-title(sprintf('Error between Measured and Target Displacements from dSpace: Actuator %02d',actID));
-legend('target','measured','error');
+xlabel('Time [sec]','FontWeight','bold');
+ylabel('Displacement [in.]','FontWeight','bold');
+title(sprintf('Error between Measured and Target Displacements from dSpace: DOF %02d',dofID),'FontWeight','bold');
+legend('target','measured',sprintf('measured (shifted by %1.0f msec)',1000*timeShift),'error');
 %==========================================================================
 % fft of error between measured and target displacements
-CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
+figure;
+%CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    targID = find(diff(atTarget) > 0) + 1;
+    targID = find(counter == max(iDelay(dofID),1));
     tID = targID >= id; tID(1) = 0;
     targID = targID(tID);
     t = time(targID);
-    error = measDsp(targID,actID) - targDsp(targID,actID);
+    error = measDsp(targID,dofID) - targDsp(targID,dofID);
     tIP = linspace(t(1),t(end),length(t))';
     errorIP = interp1(t,error,tIP,'linear');
     dt = tIP(2) - tIP(1);
-    getFFT(errorIP,dt,sprintf('Error between Measured and Target Displacements: Actuator %02d',actID));
+    getFFT(errorIP,dt,sprintf('Error between Measured and Target Displacements: DOF %02d',dofID));
 end
 %==========================================================================
 % subspace plot of measured vs. target displacements
-CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
+figure;
+%CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    targID = find(diff(atTarget) > 0) + 1;
+    targID = find(counter == max(iDelay(dofID),1));
     tID = targID >= id;
     targID = targID(tID);
-    plot(targDsp(targID,actID),measDsp(targID,actID),'-b');
+    plot(targDsp(targID,dofID),measDsp(targID,dofID),'-b','LineWidth',1.5);
     hold('on');
 end
 grid('on');
-xlabel('Target Displacement [in.]');
-ylabel('Measured Displacement [in.]');
-title(sprintf('Subspace Plot of Measured vs. Target Displacements from dSpace: Actuator %02d',actID));
+xlabel('Target Displacement [in.]','FontWeight','bold');
+ylabel('Measured Displacement [in.]','FontWeight','bold');
+title(sprintf('Subspace Plot of Measured vs. Target Displacements from dSpace: DOF %02d',dofID),'FontWeight','bold');
 %==========================================================================
 % Mercan (2007) tracking indicator of measured vs. target displacements
-CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
+figure;
+%CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    targID = find(diff(atTarget) > 0) + 1;
+    targID = find(counter == max(iDelay(dofID),1));
     tID = targID >= id;
     targID = targID(tID);
-    A  = cumsum(0.5*(measDsp(targID(2:end),actID)+measDsp(targID(1:end-1),actID)).*diff(targDsp(targID,actID)));
-    TA = cumsum(0.5*(targDsp(targID(2:end),actID)+targDsp(targID(1:end-1),actID)).*diff(measDsp(targID,actID)));
+    A  = cumsum(0.5*(measDsp(targID(2:end),dofID)+measDsp(targID(1:end-1),dofID)).*diff(targDsp(targID,dofID)));
+    TA = cumsum(0.5*(targDsp(targID(2:end),dofID)+targDsp(targID(1:end-1),dofID)).*diff(measDsp(targID,dofID)));
     TI = 0.5*(A - TA);
     TI = [0;TI];
     t = time(targID);
-    plot(t,TI,'-b');
+    plot(t,TI,'-b','LineWidth',1.5);
     hold('on');
 end
 grid('on');
-xlabel('Time [sec]');
-ylabel('Tracking Indicator [in^2]');
-title(sprintf('Tracking Indicator from dSpace: Actuator %02d',actID));
+xlabel('Time [sec]','FontWeight','bold');
+ylabel('Tracking Indicator [in^2]','FontWeight','bold');
+title(sprintf('Tracking Indicator from dSpace: DOF %02d',dofID),'FontWeight','bold');
 %==========================================================================
 % fft of error between measured and commmand displacements
 %CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 %try
-%    error = measDsp(id:end,actID) - commDsp(id:end,actID);
+%    error = measDsp(id:end,dofID) - commDsp(id:end,dofID);
 %    dt = 1/1000;
-%    getFFT(error,dt,sprintf('Error between Measured and Command Displacements: Actuator %02d',actID));
+%    getFFT(error,dt,sprintf('Error between Measured and Command Displacements: DOF %02d',dofID));
 %end
 %==========================================================================
 % measured force
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    plot(time(id:end),measFrc(id:end,actID),'-');
+    plot(time(id:end),measFrc(id:end,dofID),'-b','LineWidth',1.0);
 end
 grid('on');
 xlabel('Time [sec]');
 ylabel('Measured Force [kip]');
-title(sprintf('Measured Force from dSpace: Actuator %02d',actID));
+title(sprintf('Measured Force from dSpace: DOF %02d',dofID));
 %==========================================================================
 % fft of measured force
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
     dt = 1/1000;
-    getFFT(measFrc(id:end,actID),dt,sprintf('Measured Force: Actuator %02d',actID));
+    getFFT(measFrc(id:end,dofID),dt,sprintf('Measured Force: DOF %02d',dofID));
     set(gca,'YScale','log');
 end
 %==========================================================================
 % state of predictor-corrector
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    plot(time(id:end),state(id:end),'-b');
+    plot(time(id:end),state(id:end),'-b','LineWidth',1.0);
 end
 grid('on');
 xlabel('Time [sec]');
@@ -188,7 +240,7 @@ title('State of Predictor-Corrector from dSpace');
 % counter of predictor-corrector
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    plot(time(id:end),counter(id:end),'-b');
+    plot(time(id:end),counter(id:end),'-b','LineWidth',1.0);
 end
 grid('on');
 xlabel('Time [sec]');
@@ -198,14 +250,14 @@ title('Counter of Predictor-Corrector from dSpace');
 % flags of predictor-corrector
 CreateWindow('cen',0.80*SS(4)/3*4,0.80*SS(4));
 try
-    plot(time(id:end),newTarget(id:end),'-b');
+    plot(time(id:end),newTarget(id:end),'-b','LineWidth',1.0);
 end
 hold('on');
 try
-    plot(time(id:end),switchPC(id:end),'--r');
+    plot(time(id:end),switchPC(id:end),'--r','LineWidth',1.0);
 end
 try
-    plot(time(id:end),atTarget(id:end),'-.g');
+    plot(time(id:end),atTarget(id:end),'-.g','LineWidth',1.0);
 end
 grid('on');
 xlabel('Time [sec]');
