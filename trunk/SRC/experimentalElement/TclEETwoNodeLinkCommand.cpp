151e9fb228edcc0b9dc1f369ad22a79879dda126
aschell <aschell@187af744-7923-0410-8f01-f88a99ace7d3>
2016-10-12 23:34:32 +0000

    clean up

--- trunk/SRC/experimentalElement/TclEETwoNodeLinkCommand.cpp
+++ trunk/SRC/experimentalElement/TclEETwoNodeLinkCommand.cpp
@@ -49,7 +49,7 @@ static void printCommand(int argc, TCL_Char **argv)
     for (int i=0; i<argc; i++)
         opserr << argv[i] << " ";
     opserr << endln;
-} 
+}
 
 
 int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
@@ -66,7 +66,7 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
         opserr << "Want: expElement twoNodeLink eleTag iNode jNode -dir dirs -site siteTag -initStif Kij <-orient <x1 x2 x3> y1 y2 y3> <-pDelta Mratios> <-shearDist sDratios> <-iMod> <-noRayleigh> <-mass m>\n";
         opserr << "  or: expElement twoNodeLink eleTag iNode jNode -dir dirs -server ipPort <ipAddr> <-ssl> <-udp> <-dataSize size> -initStif Kij <-orient <x1 x2 x3> y1 y2 y3> <-pDelta Mratios> <-shearDist sDratios> <-iMod> <-noRayleigh> <-mass m>\n";
         return TCL_ERROR;
-    }    
+    }
     
     // get the id and end nodes 
     int tag, iNode, jNode, siteTag, numDir, dirID, ipPort, argi, i, j, k;
@@ -77,7 +77,7 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
     Vector Mratio(0), shearDistI(0);
     bool iMod = false;
     int doRayleigh = 1;
-	double mass = 0.0;
+    double mass = 0.0;
     
     if (Tcl_GetInt(interp, argv[1+eleArgStart], &tag) != TCL_OK)  {
         opserr << "WARNING invalid expElement twoNodeLink eleTag\n";
@@ -98,34 +98,34 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
     if (strcmp(argv[4+eleArgStart],"-dir") != 0)  {
         opserr << "WARNING expecting -dir flag\n";
         opserr << "expElement twoNodeLink element: " << tag << endln;
-        return TCL_ERROR;	
+        return TCL_ERROR;
     }
     argi = 5+eleArgStart;
     i = argi;
     while (i < argc &&
         strcmp(argv[i],"-site") != 0 && 
         strcmp(argv[i],"-server") != 0)  {
-        numDir++;
-        i++;
+            numDir++;
+            i++;
     }
     if (numDir == 0)  {
-		opserr << "WARNING no directions specified\n";
-		opserr << "expElement twoNodeLink element: " << tag << endln;
-		return TCL_ERROR;
-	}
+        opserr << "WARNING no directions specified\n";
+        opserr << "expElement twoNodeLink element: " << tag << endln;
+        return TCL_ERROR;
+    }
     // create the ID array to hold the direction IDs
     ID theDirIDs(numDir);
     // fill in the directions
     for (i=0; i<numDir; i++)  {
         if (Tcl_GetInt(interp, argv[argi], &dirID) != TCL_OK)  {
             opserr << "WARNING invalid direction ID\n";
-            opserr << "expElement twoNodeLink element: " << tag << endln;	    
+            opserr << "expElement twoNodeLink element: " << tag << endln;
             return TCL_ERROR;
         }
         if (dirID < 1 || dirID > ndf)  {
             opserr << "WARNING invalid direction ID: ";
             opserr << "dir = " << dirID << " > ndf = " << ndf;
-            opserr << "\nexpElement twoNodeLink element: " << tag << endln;	    
+            opserr << "\nexpElement twoNodeLink element: " << tag << endln;
             return TCL_ERROR;
         }
         theDirIDs(i) = dirID-1;
@@ -133,35 +133,35 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
     }
     if (strcmp(argv[argi], "-site") == 0)  {
         argi++;
-	    if (Tcl_GetInt(interp, argv[argi], &siteTag) != TCL_OK)  {
-		    opserr << "WARNING invalid siteTag\n";
-		    opserr << "expElement twoNodeLink element: " << tag << endln;
-		    return TCL_ERROR;
-	    }
+        if (Tcl_GetInt(interp, argv[argi], &siteTag) != TCL_OK)  {
+            opserr << "WARNING invalid siteTag\n";
+            opserr << "expElement twoNodeLink element: " << tag << endln;
+            return TCL_ERROR;
+        }
         argi++;
-	    theSite = getExperimentalSite(siteTag);
-	    if (theSite == 0)  {
-		    opserr << "WARNING experimental site not found\n";
-		    opserr << "expSite: " << siteTag << endln;
-		    opserr << "expElement twoNodeLink element: " << tag << endln;
-		    return TCL_ERROR;
-	    }
+        theSite = getExperimentalSite(siteTag);
+        if (theSite == 0)  {
+            opserr << "WARNING experimental site not found\n";
+            opserr << "expSite: " << siteTag << endln;
+            opserr << "expElement twoNodeLink element: " << tag << endln;
+            return TCL_ERROR;
+        }
     }
     else if (strcmp(argv[argi], "-server") == 0)  {
         argi++;
         if (Tcl_GetInt(interp, argv[argi], &ipPort) != TCL_OK)  {
-	        opserr << "WARNING invalid ipPort\n";
-	        opserr << "expElement twoNodeLink element: " << tag << endln;
-	        return TCL_ERROR;
+            opserr << "WARNING invalid ipPort\n";
+            opserr << "expElement twoNodeLink element: " << tag << endln;
+            return TCL_ERROR;
         }
         argi++;
         if (strcmp(argv[argi], "-initStif") != 0 &&
             strcmp(argv[argi], "-ssl") != 0 &&
             strcmp(argv[argi], "-udp") != 0 &&
             strcmp(argv[argi], "-dataSize") != 0)  {
-            ipAddr = new char [strlen(argv[argi])+1];
-            strcpy(ipAddr,argv[argi]);
-            argi++;
+                ipAddr = new char [strlen(argv[argi])+1];
+                strcpy(ipAddr,argv[argi]);
+                argi++;
         }
         else  {
             ipAddr = new char [9+1];
@@ -174,17 +174,17 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
                 udp = 1;
             else if (strcmp(argv[i], "-dataSize") == 0)  {
                 if (Tcl_GetInt(interp, argv[i+1], &dataSize) != TCL_OK)  {
-		            opserr << "WARNING invalid dataSize\n";
-		            opserr << "expElement twoNodeLink element: " << tag << endln;
-		            return TCL_ERROR;
-	            }
+                    opserr << "WARNING invalid dataSize\n";
+                    opserr << "expElement twoNodeLink element: " << tag << endln;
+                    return TCL_ERROR;
+                }
             }
         }
     }
     else  {
         opserr << "WARNING expecting -site or -server string but got ";
         opserr << argv[argi] << endln;
-	    opserr << "expElement twoNodeLink element: " << tag << endln;
+        opserr << "expElement twoNodeLink element: " << tag << endln;
         return TCL_ERROR;
     }
     // check for optional arguments
@@ -200,8 +200,8 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
                 strcmp(argv[j],"-doRayleigh") != 0 &&
                 strcmp(argv[j],"-noRayleigh") != 0 &&
                 strcmp(argv[j],"-mass") != 0)  {
-                numOrient++;
-                j++;
+                    numOrient++;
+                    j++;
             }
             if (numOrient == 3)  {
                 y.resize(3);
@@ -238,7 +238,7 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
                         opserr << "expElement twoNodeLink element: " << tag << endln;
                         return TCL_ERROR;
                     } else  {
-                        y(j) = value;		
+                        y(j) = value;
                     }
                 }
             }
@@ -249,34 +249,34 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
             }
         }
     }
-	for (i=argi; i<argc; i++)  {
-		if (i+1 < argc && strcmp(argv[i], "-pDelta") == 0)  {
+    for (i=argi; i<argc; i++)  {
+        if (i+1 < argc && strcmp(argv[i], "-pDelta") == 0)  {
             double Mr;
             Mratio.resize(4);
             if (ndm == 2)  {
                 Mratio.Zero();
                 for (j=0; j<2; j++)  {
-			        if (Tcl_GetDouble(interp, argv[i+1+j], &Mr) != TCL_OK)  {
-				        opserr << "WARNING invalid -pDelta value\n";
-				        opserr << "expElement twoNodeLink element: " << tag << endln;
-				        return TCL_ERROR;
+                    if (Tcl_GetDouble(interp, argv[i+1+j], &Mr) != TCL_OK)  {
+                        opserr << "WARNING invalid -pDelta value\n";
+                        opserr << "expElement twoNodeLink element: " << tag << endln;
+                        return TCL_ERROR;
                     }
                     Mratio(2+j) = Mr;
                 }
             } else if (ndm == 3)  {
                 for (j=0; j<4; j++)  {
-			        if (Tcl_GetDouble(interp, argv[i+1+j], &Mr) != TCL_OK)  {
-				        opserr << "WARNING invalid -pDelta value\n";
-				        opserr << "expElement twoNodeLink element: " << tag << endln;
-				        return TCL_ERROR;
+                    if (Tcl_GetDouble(interp, argv[i+1+j], &Mr) != TCL_OK)  {
+                        opserr << "WARNING invalid -pDelta value\n";
+                        opserr << "expElement twoNodeLink element: " << tag << endln;
+                        return TCL_ERROR;
                     }
                     Mratio(j) = Mr;
                 }
             }
-		}
-	}
-	for (i=argi; i<argc; i++)  {
-		if (i+1 < argc && strcmp(argv[i], "-shearDist") == 0)  {
+        }
+    }
+    for (i=argi; i<argc; i++)  {
+        if (i+1 < argc && strcmp(argv[i], "-shearDist") == 0)  {
             double sDI;
             shearDistI.resize(2);
             if (ndm == 2)  {
@@ -289,16 +289,16 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
                 shearDistI(1) = 0.5;
             } else if (ndm == 3)  {
                 for (j=0; j<2; j++)  {
-			        if (Tcl_GetDouble(interp, argv[i+1+j], &sDI) != TCL_OK)  {
-				        opserr << "WARNING invalid -shearDist value\n";
-				        opserr << "expElement twoNodeLink element: " << tag << endln;
-				        return TCL_ERROR;
+                    if (Tcl_GetDouble(interp, argv[i+1+j], &sDI) != TCL_OK)  {
+                        opserr << "WARNING invalid -shearDist value\n";
+                        opserr << "expElement twoNodeLink element: " << tag << endln;
+                        return TCL_ERROR;
                     }
                     shearDistI(j) = sDI;
                 }
             }
-		}
-	}
+        }
+    }
     for (i=argi; i<argc; i++)  {
         if (strcmp(argv[i], "-iMod") == 0)  {
             iMod = true;
@@ -311,15 +311,15 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
             doRayleigh = 0;
         }
     }
-	for (i=argi; i<argc; i++)  {
-		if (i+1 < argc && strcmp(argv[i], "-mass") == 0)  {
-			if (Tcl_GetDouble(interp, argv[i+1], &mass) != TCL_OK)  {
-				opserr << "WARNING invalid -mass value\n";
-				opserr << "expElement twoNodeLink element: " << tag << endln;
-				return TCL_ERROR;
-			}
-		}
-	}
+    for (i=argi; i<argc; i++)  {
+        if (i+1 < argc && strcmp(argv[i], "-mass") == 0)  {
+            if (Tcl_GetDouble(interp, argv[i+1], &mass) != TCL_OK)  {
+                opserr << "WARNING invalid -mass value\n";
+                opserr << "expElement twoNodeLink element: " << tag << endln;
+                return TCL_ERROR;
+            }
+        }
+    }
     // now create the EETwoNodeLink
     if (theSite != 0)  {
         theExpElement = new EETwoNodeLink(tag, ndm, iNode, jNode, theDirIDs,
@@ -330,44 +330,44 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
             iMod, doRayleigh, mass);
     }
     
-	if (theExpElement == 0)  {
-		opserr << "WARNING ran out of memory creating element\n";
-		opserr << "expElement twoNodeLink element: " << tag << endln;
-		return TCL_ERROR;
-	}
+    if (theExpElement == 0)  {
+        opserr << "WARNING ran out of memory creating element\n";
+        opserr << "expElement twoNodeLink element: " << tag << endln;
+        return TCL_ERROR;
+    }
     
-	// then add the EETwoNodeLink to the domain
-	if (theTclDomain->addElement(theExpElement) == false)  {
-		opserr << "WARNING could not add element to the domain\n";
-		opserr << "expElement twoNodeLink element: " << tag << endln;
-		delete theExpElement;
-		return TCL_ERROR;
-	}
+    // then add the EETwoNodeLink to the domain
+    if (theTclDomain->addElement(theExpElement) == false)  {
+        opserr << "WARNING could not add element to the domain\n";
+        opserr << "expElement twoNodeLink element: " << tag << endln;
+        delete theExpElement;
+        return TCL_ERROR;
+    }
     
-	// finally check for initial stiffness terms
+    // finally check for initial stiffness terms
     int setInitStif = -1;
-	for (i = argi; i < argc; i++)  {
-		if (strcmp(argv[i], "-initStif") == 0)  {
-			if (argc-1 < i+numDir*numDir)  {
-				opserr << "WARNING incorrect number of inital stiffness terms\n";
-				opserr << "expElement twoNodeLink element: " << tag << endln;
-				return TCL_ERROR;      
-			}
-			Matrix theInitStif(numDir,numDir);
-			double stif;
-			for (j=0; j<numDir; j++)  {
-				for (k=0; k<numDir; k++)  {
-					if (Tcl_GetDouble(interp, argv[i+1 + numDir*j+k], &stif) != TCL_OK)  {
-						opserr << "WARNING invalid initial stiffness term\n";
-						opserr << "expElement twoNodeLink element: " << tag << endln;
-						return TCL_ERROR;
-					}
-					theInitStif(j,k) = stif;
-				}
-			}
-			setInitStif = theExpElement->setInitialStiff(theInitStif);
-		}
-	}
+    for (i = argi; i < argc; i++)  {
+        if (strcmp(argv[i], "-initStif") == 0)  {
+            if (argc-1 < i+numDir*numDir)  {
+                opserr << "WARNING incorrect number of inital stiffness terms\n";
+                opserr << "expElement twoNodeLink element: " << tag << endln;
+                return TCL_ERROR;      
+            }
+            Matrix theInitStif(numDir,numDir);
+            double stif;
+            for (j=0; j<numDir; j++)  {
+                for (k=0; k<numDir; k++)  {
+                    if (Tcl_GetDouble(interp, argv[i+1 + numDir*j+k], &stif) != TCL_OK)  {
+                        opserr << "WARNING invalid initial stiffness term\n";
+                        opserr << "expElement twoNodeLink element: " << tag << endln;
+                        return TCL_ERROR;
+                    }
+                    theInitStif(j,k) = stif;
+                }
+            }
+            setInitStif = theExpElement->setInitialStiff(theInitStif);
+        }
+    }
     if (setInitStif != 0)  {
         opserr << "WARNING initial stiffness not set\n";
         opserr << "expElement twoNodeLink element: " << tag << endln;
@@ -375,5 +375,5 @@ int addEETwoNodeLink(ClientData clientData, Tcl_Interp *interp, int argc,
     }
     
     // if get here we have sucessfully created the EETwoNodeLink and added it to the domain
-	return TCL_OK;
+    return TCL_OK;
 }
