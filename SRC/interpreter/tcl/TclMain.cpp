0fd5412b21505abdb9d5f5fc77ec4f0020f5632c
aschellenberg74 <andreas.schellenberg@gmail.com>
2021-06-28 07:00:13 -0700

    cleanup of Tcl and Python interpreter commands and required methods

---
+++ SRC/interpreter/tcl/TclMain.cpp
@@ -0,0 +1,37 @@
+/* ****************************************************************** **
+**    OpenFRESCO - Open Framework                                     **
+**                 for Experimental Setup and Control                 **
+**                                                                    **
+**                                                                    **
+** Copyright (c) 2006, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited. See    **
+** file 'COPYRIGHT_UCB' in main directory for information on usage    **
+** and redistribution, and for a DISCLAIMER OF ALL WARRANTIES.        **
+**                                                                    **
+** Developed by:                                                      **
+**   Andreas Schellenberg (andreas.schellenberg@gmx.net)              **
+**   Yoshikazu Takahashi (yos@catfish.dpri.kyoto-u.ac.jp)             **
+**   Gregory L. Fenves (fenves@berkeley.edu)                          **
+**   Stephen A. Mahin (mahin@berkeley.edu)                            **
+**                                                                    **
+** ****************************************************************** */
+
+// Written: Andreas Schellenberg (andreas.schellenberg@gmail.com)
+// Created: 01/21
+// Revision: A
+//
+// Description: this is the main for the TclInterpreter
+
+#include "TclInterpreter.h"
+
+int main(int argc, char** argv) {
+    TclInterpreter* theInterpreter = new TclInterpreter(argc, argv);
+    int res = theInterpreter->run();
+    
+    delete theInterpreter;
+    
+    return res;
+}
